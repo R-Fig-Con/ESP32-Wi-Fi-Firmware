bdc11510f8c20df5e23593cbef665234141f7f42
eduardoisel <eduardodinis3@gmail.com>
2025-04-22 17:11:56 +0100

    tx and rx on both, cant receive during difs wait
    Implementation needs to be able to notice incoming packets when waiting to transmit itself. Probably needs usage of tasks

--- wifi_firmware_esp32/wifi_firmware_esp32.ino
+++ wifi_firmware_esp32/wifi_firmware_esp32.ino
@@ -6,37 +6,54 @@
 #include "src/traffic_generator/traffic_generator.h"
 #include "src/csma_control/csma_control.h"
 
-enum nodeOpMode {
-
-  NODE_OP_MODE_RECEIVER,
-  NODE_OP_MODE_TRANSMITTER
-  
-};
-
 CC1101 radio;
 
 byte syncWord[2] = {199, 10};
 volatile bool packetWaiting;
 
-nodeOpMode opMode;
 uint8_t myMacAddress[6];
-CCPACKET packet_to_send;
+
+// Packet and frame used by the sender.
+TRAFFIC_GEN * trf_gen;
+
+CSMA_CONTROL * csma_control;
 
 
-unsigned long time_marker = 0;
+TaskHandle_t receiveHandle = NULL;
 
 void messageReceived() {
-    time_marker = micros();
-    //Serial.printf("ON INTERRUPT marker = %lu\n", time_marker);//CAUSES FAILURE???
     packetWaiting = true;
+    /*
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+    vTaskNotifyGiveFromISR(receiveHandle, &xHigherPriorityTaskWoken);
+    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+    */
+
+    //vTaskResume(receiveHandle);
 }
 
+void receiveTask(void* unused_param){
+
+  while(true){
+
+    vTaskSuspend(receiveHandle);
+
+    receiver();
+
+    vTaskDelay(1000 / portTICK_PERIOD_MS); // Pause before next check
+
+  }
+      
+}
+
+bool checkChannel(){  
+  return radio.cca();
+}
 
 void setup() {
+
     // Serial communication for debug
     Serial.begin(115200);
-
-    delay(1000);
 
     // Wifi, for getting the MAC address.
     WiFi.mode(WIFI_STA);
@@ -62,159 +79,130 @@ void setup() {
 
     Serial.println(F("CC1101 radio initialized."));
 
-    // Select this node's operation mode
-    opMode = NODE_OP_MODE_RECEIVER;
-    //opMode = NODE_OP_MODE_TRANSMITTER;
+    csma_control = new CSMA_CONTROL(&checkChannel);
 
-    if (opMode == NODE_OP_MODE_RECEIVER) Serial.printf("Works as receiver, marker: %lu\n", time_marker);
-    else {sender_create_data_packet(&packet_to_send); Serial.printf("Works as receiver\n");}
+
+    attachInterrupt(CC1101_GDO0, messageReceived, RISING);
+    
+    uint8_t dstMacAddress[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+    trf_gen = new TRAFFIC_GEN(&sender, myMacAddress, dstMacAddress);
+    trf_gen->setTime(TRF_GEN_GAUSS, 6000);
+    
+    /*
+    //creating when there is nothing to receive may cause troubles
+    xTaskCreatePinnedToCore(
+      &receiveTask,
+      "receive and send acknowledge",
+      10000, //no thought used to decide size
+      NULL,
+      10, // believe it should be bigger than traffic generation
+      &receiveHandle,
+      1 //putting related to cc1101 on same core
+    );
+    */
 
     // Make sure the radio is on RX.
-    radio.setRxState();
-
-    #ifdef ESP32
-    Serial.println("ESP32 is defined");
-
-    #else
-    Serial.println(F("Not defined"));
-    #endif 
+    radio.setRxState(); //TODO Maybe change to do this in constructor/init
 }
 
-CCPACKET packet_to_receive;//also to send
+TaskHandle_t generatorHandle = NULL;
+
+void generatorTask(void* unusedParam){
+  trf_gen->init();//is already a loop. Maybe change to add init restart on isRunning == false?
+}
+
 void loop(){
-  
-    if(opMode == NODE_OP_MODE_TRANSMITTER){
-        sendAndReceive();
-        delay(40000);
-    }else if(opMode == NODE_OP_MODE_RECEIVER){
-        receiveAndAcknowledge();
+    if(!trf_gen->isRunning()){
+        Serial.println(F("Initiating traffic...")); 
+        xTaskCreatePinnedToCore(
+          &generatorTask,     // Function to execute
+          "traffic generator",   // Name of the task
+          10000,      // Stack size
+          NULL,      // Task parameters
+          1,         // Priority
+          &generatorHandle,      // Task handle
+          1          // Core 1
+        );
     }
-    
 }
 
-
+CCPACKET packet_to_receive;
 ieeeFrame * frame = (ieeeFrame *) packet_to_receive.data;
 
-void receiveAndAcknowledge(){
-  attachInterrupt(CC1101_GDO0, messageReceived, RISING);
-  Serial.println(F("TO WAIT FOR PACKET"));
-  while(!packetWaiting);
-  receiver();
+/**
+ * requires radio initialization, csma control instance, receive task creation
+ * Contains retry logic for now at least
+ */
+void sender(CCPACKET packet_to_send) { 
 
-  packet_to_receive.length = 0;
-  PACKET_TO_ACK(frame);
+  uint8_t retryCount = 0;
 
+  send:
 
-  radio.sendData(packet_to_receive);
-  //radio.sendWithWaitPrint(packet_to_receive);
-
-  Serial.printf("PACKET SENT, state: %d in core %d\n", (int) radio.readStatusReg(CC1101_MARCSTATE), xPortGetCoreID());
-
-}
-
-
-void sender_create_data_packet(CCPACKET * packet) {
-  unsigned int seqNum = 0;
-  ieeeFrame * f = (ieeeFrame *) packet->data;
-
-  // Fill the MAC address.
-  memcpy(f->addr_src, myMacAddress, 6);
-
-  // Fill the payload. Start by adding a sequence number.
-  // TODO: We will certainly want this sequence number to be 
-  // binary in the final version. We are using strings
-  // here just ti simplify debug.
-  sprintf((char *) f->payload, "%08X ", seqNum++);
-
-  // Now, fill the remainder of the payload with some 
-  // data (just to have something that is verificable on
-  // the receiver end).
-  // TODO: this can be safely removed on the final version,
-  // as the payload itself is not going to be important.
-  int l = strlen((char *) f->payload);
-  int i;
-  for (i = 0; i < 1000; i++) {
-
-    f->payload[l++] = (char) ((i % 42) + 48);
-  }
-  f->payload[l] = (char) 0;
-  
-  // Set the packet length. It is the length of the payload
-  // because it is a string, remember to count the \0 at the end,
-  // plus the 6 bytes of the MAC address.
-  packet->length = strlen((char *) f->payload)  + 1 + 6;
-}
- 
-void sendAndReceive(){
-  Serial.printf("Send and receive, marker: %lu\n", time_marker);
-  radio.sendData(packet_to_send);
-  unsigned long wait_start = micros();
-  //Serial.printf("2nd Send and receive, marker: %lu\n", time_marker);
-  attachInterrupt(CC1101_GDO0, messageReceived, RISING);
-
-  unsigned long c = 0;
-  while(!packetWaiting){
-    Serial.println(F("LOOP\n"));//Necesary to detect? TODO CHECK
-    c += 1;
-  }
-
-  Serial.printf("Initial sender; Took %lu microsseconds from packet send to answer, loop count %lu;\n", time_marker - wait_start, c);
-
-  time_marker = 0;
   detachInterrupt(CC1101_GDO0);
 
+  if(retryCount == 10){
+    Serial.println(F("GIVING UP after retry limit reached"));
+    return;
+  }
+
+  csma_control->waitForTurn();
+
+  bool b = radio.sendData(packet_to_send);
+  if(!b){//in protocol should not expect failure here
+    Serial.println(F("Send failed."));
+    retryCount += 1;
+    goto send;
+  }
+
+  unsigned long start_time = micros();
+  Serial.println(F("Packet sent"));
+
+  attachInterrupt(CC1101_GDO0, messageReceived, RISING);
+
+  while(!packetWaiting){
+    //sifs wait
+    if(micros() - start_time >= SIFS){
+      retryCount += 1;
+      goto send;
+    }
+
+  }
+
+
+  receiver();
+
+  //checks if is ack
+  if(!PACKET_IS_ACK(frame)){
+    retryCount += 1;
+    goto send;
+  }
+
+  //to eventually add sucess to register, so it can be accessed by the esp wifi
+
+  Serial.println(F("Complete success"));
 }
 
+/**
+ * Directly affects 'packet_to_receive' global var
+ */
 void receiver() {
 
   // Yes. Disable the reception interruption while we handle this packet.
   detachInterrupt(CC1101_GDO0);
 
   // Try to receive the packet
-  if (radio.receiveData(&packet_to_receive) > 0) {
+  radio.receiveData(&packet_to_receive);
 
-    //Serial.println(F("Received packet..."));
+  Serial.println(F("Received packet..."));
 
-    // We received something, but is the packet correct?
-    if (!packet_to_receive.crc_ok) {
-        Serial.println(F("crc not ok"));
-    }
-
-    /*
-    Serial.printf("STATE ON MAIN BEFORE OTHER PRINTS: %d\n", (int) radio.readStatusReg(CC1101_MARCSTATE));
-
-    // Print some physical layer statistics
-    Serial.print(F("lqi: "));
-    Serial.println(radio.raw2lqi(packet_to_receive.lqi));
-    Serial.print(F("rssi: "));
-    Serial.print(radio.raw2rssi(packet_to_receive.rssi));
-    Serial.println(F("dBm"));
-
-    // If the packet seems right, let's process it.
-    if (packet_to_receive.length > 0) {
-
-      // Just print some debug info.
-      Serial.print(F("packet: len "));
-      Serial.println(packet_to_receive.length);
-      Serial.printf("src: %02X:%02X:%02X:%02X:%02X:%02X\n", 
-            frame->addr_src[0],
-            frame->addr_src[1],
-            frame->addr_src[2],
-            frame->addr_src[3],
-            frame->addr_src[4],
-            frame->addr_src[5]);
-      Serial.println(F("data: "));
-      Serial.println((const char *) frame->payload);
-    }
-    
-
-    Serial.printf("STATE ON MAIN AFTER OTHER PRINTS: %d\n", (int) radio.readStatusReg(CC1101_MARCSTATE));
-
-    */
-
+  // We received something, but is the packet correct?
+  if (!packet_to_receive.crc_ok) {
+      Serial.println(F("crc not ok"));
   }
-
+  
   // Mark the packet as processed and re-enable the interruption.
   packetWaiting = false;
+  attachInterrupt(CC1101_GDO0, messageReceived, RISING);
 
 }
